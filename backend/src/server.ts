a5668956615ce5f181dc50f937a5c90653c4fd62
Angel <ar2754072@gmail.com>
2026-02-17 14:27:48 -0600

    feat: Implement video streaming and storage via WebSocket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,23 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import rabbitMQConnector from './config/rabbitmq';
-import redisPlugin from './config/redis';
+//import rabbitMQConnector from './config/rabbitmq';
+//import redisPlugin from './config/redis';
 import { ProducerService } from './broker/producer.service';
 import { userRoutes } from './modules/user/user.routes';
+import {videoRoutes} from './modules/monitoreo/video/video.routes';
+import websocket from '@fastify/websocket';
 
-export let broker : ProducerService; //Variable global para acceder al broker
+export let broker: ProducerService; //Variable global para acceder al broker
 
 export const buildServer = (): FastifyInstance => {
     const server = Fastify({
         logger: true
     });
 
-    server.register(rabbitMQConnector); //conexion a rabbitMQ
-    server.register(redisPlugin); //conexion a redis
+    //server.register(rabbitMQConnector); //conexion a rabbitMQ
+    //server.register(redisPlugin); //conexion a redis
+    server.register(websocket);
     server.register(userRoutes, { prefix: '/api' });
+    server.register(videoRoutes, { prefix: '/api/monitoreo' });
 
     server.ready().then(() => {
         broker = new ProducerService(server); //Inicializamos el broker
