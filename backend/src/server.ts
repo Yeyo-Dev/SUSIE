0f86b5677e641f6845b20f9fb30193e734b35bbb
Angel <ar2754072@gmail.com>
2026-03-02 12:52:31 -0600

    server actualizado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import Fastify, { FastifyInstance } from 'fastify';
 import cors from '@fastify/cors';
-import rabbitMQConnector from './config/rabbitmq';
+//import rabbitMQConnector from './config/rabbitmq';
 //import redisPlugin from './config/redis';
 import { ProducerService } from './broker/producer.service';
 import { userRoutes } from './modules/user/user.routes';
+import { sesionEvaluacionRoutes } from './modules/sesiones_evaluacion/sesion.routes';
 import {snapshotRoutes} from './modules/monitoreo/snapshots/snapshot.routes';
 import {audioRoutes} from './modules/monitoreo/audios/audio.routes';
 import websocket from '@fastify/websocket';
@@ -18,13 +19,19 @@ export const buildServer = (): FastifyInstance => {
     server.register(cors, {//permite la comunicacion entre el frontend y el backend
         origin: true,
     });
+    
+    // Sobrescribe  el metodo toJSON de BigInt para evitar errores de serialización al devolver objetos que contienen BigInt en las respuestas JSON.
+    (BigInt.prototype as any).toJSON = function () {
+        return this.toString();
+    };
 
     const prefixApi = '/susie/api/v1';
 
-    server.register(rabbitMQConnector); //conexion a rabbitMQ
+    //server.register(rabbitMQConnector); //conexion a rabbitMQ
     //server.register(redisPlugin); //conexion a redis
     server.register(websocket);
     server.register(userRoutes, { prefix: prefixApi + '/user' });
+    server.register(sesionEvaluacionRoutes, { prefix: prefixApi + '/sesiones' });
     server.register(snapshotRoutes, { prefix: prefixApi + '/monitoreo/evidencias' });
     server.register(audioRoutes, { prefix: prefixApi + '/monitoreo/evidencias' });
 
