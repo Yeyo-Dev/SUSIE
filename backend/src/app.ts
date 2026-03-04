f9c288b370705e282290bb074b3f6a2469478c0e
Angel <ar2754072@gmail.com>
2026-03-03 21:31:11 -0600

    update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
+
 import dotenv from 'dotenv';
 import { buildServer } from './server.js';
 
 dotenv.config();
-const app = buildServer(); // Inicializamos el servidor
 
 const startApp = async () => {
+
+    const app = buildServer();
 
     try {
         const port = parseInt(process.env.PORT || '8000');
@@ -12,13 +14,7 @@ const startApp = async () => {
 
         await app.listen({ port, host });
 
-        console.log(`
-  🚀 Servidor corriendo en:
-  👉 REST API:    http://localhost:${port}/api
-  👉 Video Stream: http://localhost:${port}/api/monitoreo/video
-  👉 Audio Stream: http://localhost:${port}/api/monitoreo/audio
-  👉 Health Check: http://localhost:${port}/
-        `);
+        console.log(`Servidor corriendo en http://localhost:${port}`);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
